2f28732d4c987ac5638b8e1c46cb32715137cd63
Adeel Mahmood <adeel41@yahoo.com>
2020-07-21 15:03:29 +0100

    Fixed build script

--- build/build.ps1
+++ build/build.ps1
@@ -1,5 +1,48 @@
-. ( Join-Path ((Get-Item ($MyInvocation.MyCommand.Definition)).Directory.FullName) "common.ps1" )
+param ($Operation = "build", $Destination = "")
+
+$validOperations = "build", "copy", "run" 
+
+if ($validOperations -notcontains $Operation) {
+    Write-Host "Please provide a valid operation from one of following options:" $validOperations
+    return
+} 
+
+if ( $Operation -eq "copy" -and $Destination -eq "") {
+    Write-Host "Please provide a valid destination"
+    return
+}
+
+function GetOneFolderUp($path) {
+    return split-path -parent $path 
+}
+
+$rootFolder = GetOneFolderUp(GetOneFolderUp($MyInvocation.MyCommand.Definition))
+#$buildFolder = Join-Path $rootFolder "build"
 $srcFolder = Join-Path $rootFolder "src"
+$outFolder = Join-Path $rootFolder "out"
+
+function GetRelativePath {
+    $combinedPath = $rootFolder
+    for($i = 0; $i -lt $args.Count; $i++) {
+        $combinedPath = Join-Path $combinedPath $args[$i]
+    }
+    return $combinedPath
+}
+
+function RefreshEnvironmentPathVariable() {
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+}
+
 
 & ponyc.exe $srcFolder -b "pony-langserver" -o "out"
 $executable = Join-Path $outFolder "pony-langserver.exe"
+
+if ($Operation -eq "run") {
+    Clear-Host
+    & $executable
+    return
+}
+
+if ($Operation -eq "copy") {
+    Copy-Item $executable -Destination $Destination -Force
+}
--- build/run.ps1
+++
@@ -1,5 +0,0 @@
-. ( Join-Path ((Get-Item ($MyInvocation.MyCommand.Definition)).Directory.FullName) "common.ps1" )
-. ( Join-Path $buildFolder "build.ps1" )
-
-& clear
-& $executable
